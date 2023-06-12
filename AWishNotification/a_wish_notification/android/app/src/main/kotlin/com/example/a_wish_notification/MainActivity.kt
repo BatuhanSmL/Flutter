3f7878d94cf347ba9c51c4daa3c65f1fd04003b8
BatuhanSmL <97319451+BatuhanSmL@users.noreply.github.com>
2023-06-12 21:29:10 +0300

    AWishNotification app added.

---
+++ AWishNotification/a_wish_notification/android/app/src/main/kotlin/com/example/a_wish_notification/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.a_wish_notification
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ AWishNotification/a_wish_notification/lib/service.dart
@@ -0,0 +1,53 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:grock/grock.dart';
+import 'firebase_options.dart';
+
+class FirebaseNotificationService {
+  late final FirebaseMessaging messaging;
+
+  void settingNotification() async {
+    await messaging.requestPermission(
+      alert: true,
+      sound: true,
+      badge: true,
+    );
+  }
+
+  void connectNotification() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    messaging = FirebaseMessaging.instance;
+    messaging.setForegroundNotificationPresentationOptions(
+      alert: true,
+      sound: true,
+      badge: true,
+    );
+
+    settingNotification();
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      Grock.snackBar(
+        title: '${message.notification?.title}',
+        description: '${message.notification?.body}',
+        opacity: 0.8,
+        position: SnackbarPosition.top,
+        leading: message.notification?.android?.imageUrl == null
+            ? null
+            : Image.network(
+                '${message.notification?.android?.imageUrl}',
+                width: 120,
+                height: 120,
+              ),
+        
+        
+      );
+    });
+  }
+
+  static Future<void> backgroundMessage(RemoteMessage message) async {
+    await Firebase.initializeApp();
+  }
+}
